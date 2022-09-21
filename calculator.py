9efa9f2ba020fdf3ee3f2b04e8eb5edce68fa552
AlePPisa <aleppaulon@gmail.com>
2022-09-21 10:59:11 -0700

    Added division functionality
    Added division(x,y) to calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,6 @@
 # Claculator file, contains static methods for all calculator functionality
+
+# Division of two numbers, note: throws exception if y = 0
+def division(x: float, y: float) -> float:
+    return x/y
+
