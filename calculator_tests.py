b1da751a02339fcb33a494bf78d351713f5cbc6b
akshatsingal00@gmail.com <akshatsingal00@gmail.com>
2022-09-21 11:07:49 -0700

    Added the functioanlity to add two number and test

--- calculator_tests.py
+++ calculator_tests.py
@@ -1,11 +1,15 @@
 import unittest
-
+from calculator import *
 
 class CalculatorTests(unittest.TestCase):
     # This is a default method
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
-
+    
+    def test_addcase(self):
+        test = addition(1, 2)
+        expected = 3
+        self.assertEqual(test, expected)
 
 if __name__ == '__main__':
     unittest.main()
