b7df38fcde7579bd5ab1ecb8bc99bff89dd5aed2
AlePPisa <aleppaulon@gmail.com>
2022-09-21 10:59:58 -0700

    Added corresponding tests regarding division to calculator_tests.py
    Added tests regarding division to calculator_tests.py

--- calculator_tests.py
+++ calculator_tests.py
@@ -1,10 +1,30 @@
 import unittest
 
+import calculator
+from calculator import *
+
 
 class CalculatorTests(unittest.TestCase):
-    # This is a default method
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    # Tests for addition functionality
+    def test_division(self):
+        # Two positives
+        self.assertEqual(1, calculator.division(1, 1))
+
+        # Two negatives
+        self.assertEqual(2, calculator.division(-4, -2))
+
+        # One of each
+        self.assertEqual(-2, calculator.division(-4, 2))
+
+        # Float number
+        self.assertEqual(0.5, calculator.division(1, 2))
+
+        # Exceptions
+        with self.assertRaises(Exception):
+            calculator.division("a", 9)
+
+        with self.assertRaises(Exception):
+            calculator.division(4, 0)
 
 
 if __name__ == '__main__':
