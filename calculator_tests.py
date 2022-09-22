d8ba456ddde3a9554fb32080bbbaff88cc4f860f
Matt Dunn <mattdunn0816@gmail.com>
2022-09-21 19:20:51 -0700

    Added Unit Testing For Subtraction Implementation

--- calculator_tests.py
+++ calculator_tests.py
@@ -25,22 +25,41 @@ class CalculatorTests(unittest.TestCase):
 
         with self.assertRaises(Exception):
             calculator.division(4, 0)
-            
-            
+
     def test_addcase(self):
         test = calculator.addition(1, 2)
         expected = 3
         self.assertEqual(test, expected)
 
+    # Tests for subtraction functionality
+    def test_subtraction(self):
+        # Two positives
+        self.assertEqual(0, calculator.subtraction(1, 1))
+
+        # Two negatives
+        self.assertEqual(-2, calculator.subtraction(-4, -2))
+
+        # One of each
+        self.assertEqual(-6, calculator.subtraction(-4, 2))
+
+        # The Other Way
+        self.assertEqual(6, calculator.subtraction(4, -2))
+
+        # Exceptions
+        with self.assertRaises(Exception):
+            calculator.division("a", 9)
+
     def test_multiply(self):
-        nums = np.array([[1,1],[2,2],[10,10],[50,50],[1000,1000],[-365,256],[-420,-69],[99,-1],[17,38],[600,66]])
-        ans = [1*1,2*2,10*10,50*50,1000*1000,-365*256,-420*-69,99*-1,17*38,600*66]
+        nums = np.array([[1, 1], [2, 2], [10, 10], [50, 50], [
+                        1000, 1000], [-365, 256], [-420, -69], [99, -1], [17, 38], [600, 66]])
+        ans = [1*1, 2*2, 10*10, 50*50, 1000*1000, -
+               365*256, -420*-69, 99*-1, 17*38, 600*66]
 
         testAns = []
-        for i in range(0,len(nums)):
+        for i in range(0, len(nums)):
             num1 = nums[i][0]
             num2 = nums[i][1]
-            testAns.append(calculator.multiply(num1,num2))
+            testAns.append(calculator.multiply(num1, num2))
         self.assertEqual(ans, testAns)
 
 
